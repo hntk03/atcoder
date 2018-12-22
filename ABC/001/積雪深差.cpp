3704bc3cf8a19100e99e53862d19c6c9ecee7aa3
hntk03 <hntk03@gmail.com>
2018-12-22 17:00:32 +0900

    test

---
+++ ABC/001/積雪深差.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int H1,H2;
+	scanf("%d %d", &H1, &H2);
+	printf("%d\n", H1-H2);
+
+	return 0;
+
+}
