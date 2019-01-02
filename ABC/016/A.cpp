5a2fd7d094f098a0c04d45297ecec35175e95daf
hntk03 <hntk03@gmail.com>
2019-01-02 14:21:37 +0900

    AB 016

---
+++ ABC/016/A.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int M,D; scanf("%d %d", &M, &D);
+
+	if(M%D == 0) printf("YES\n");
+	else printf("NO\n");
+
+
+	return 0;
+
+}
---
+++ ABC/016/B.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int A,B,C; scanf("%d %d %d", &A, &B, &C);	
+	int ans = 0;
+
+	if(A+B == C && A-B != C) printf("+\n");
+	if(A+B != C && A-B == C) printf("-\n");
+	if(A+B != C && A-B != C) printf("!\n");
+	if(A+B == C && A-B == C) printf("?\n");
+
+
+
+	return 0;
+
+}
