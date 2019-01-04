d1129fd2710d02c3cfb706cb9a67ba6b62cee225
hntk03 <hntk03@gmail.com>
2019-01-04 11:16:03 +0900

    e

--- ABC/011/C.cpp
+++ ABC/011/C.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 
 
@@ -20,19 +21,21 @@ typedef vector<string> VS;
 #define REP(i,n)  FOR(i,0,n)
 
 #define NUM 3
+#define N_MAX = 300
 
 int ng[NUM];
+bool flag = false;
+int dp[N_MAX+1][N_MAX+1];
 
 void dfs(int n,int count){
 
-// printf("func:%d\n",n);
+	// printf("func:%d\n",n);
 	if(count > 99 || n < 0){
 		return ;
 	}
 	if(n == 0){
-		printf("YES\n");
-		return ;
-		// exit(0) ;
+		flag = true;
+		// return ;
 	}else if(n == ng[0] || n == ng[1] || n == ng[2]){
 		return ;
 	}
@@ -45,12 +48,17 @@ void dfs(int n,int count){
 
 int main(void){
 
+	memset(dp, -1, sizeof(dp));
+
 	int n; scanf("%d ", &n);
 
 	REP(i,NUM) scanf("%d", &ng[i]);
 
 	dfs(n,0);
-	printf("NO\n");
+	if(flag == true)
+		printf("YES\n");
+	else
+		printf("NO\n");
 
 
 	return 0;
