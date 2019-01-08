390d02d49f9206bed9cf32ff718f554ab17e1b05
hntk03 <hntk03@gmail.com>
2019-01-08 16:03:35 +0900

    023 A

---
+++ ABC/023/A.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int X; scanf("%d", &X);
+
+	printf("%d\n",X%10+X/10);
+
+
+	return 0;
+
+}
---
+++ ABC/023/B.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int n; scanf("%d", &n);
+	char s[101];
+
+	scanf("%d", &n);
+	scanf("%s", s);
+
+	if(n%2 == 0){
+		printf("-1\n");
+		return 0;
+	}
+
+	char a[101];
+	for(i,n){
+		
+
+	}
+
+
+
+
+	return 0;
+
+}
