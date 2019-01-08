3dbc0d4006a509b6c0afaf856b2a39d964bd3559
hntk03 <hntk03@gmail.com>
2019-01-08 17:41:03 +0900

    AC B

--- ABC/023/B.cpp
+++ ABC/023/B.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <string.h>
 #include <algorithm>
 
 
@@ -34,13 +35,40 @@ int main(void){
 		return 0;
 	}
 
-	char a[101];
-	for(i,n){
-		
+	char a[101] = {};
+	char b[101] = {};
+	a[0] = 'b';
 
+	int i;
+	for(i=0;strlen(a) != n;++i){
+		switch(i%3){
+			case 0:
+				sprintf(a, "%sc",a);
+				sprintf(b, "a%s",a);
+				strcpy(a,b);
+				break;
+			case 1:
+				sprintf(a, "%sa",a);
+				sprintf(b, "c%s",a);
+				strcpy(a,b);
+				break;
+			case 2:
+				sprintf(a, "%sb",a);
+				sprintf(b, "b%s",a);
+				strcpy(a,b);
+				break;
+
+			default:
+				break;
+		}
+		
 	}
 
-
+	if(strcmp(a,s) == 0){
+		printf("%d\n",i);
+	}else{
+		printf("-1\n");
+	}
 
 
 	return 0;
