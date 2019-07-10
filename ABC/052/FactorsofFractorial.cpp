75309865e6df63c6da7db8b96b0d197a8fba7aa1
hntk03 <hntk03@gmail.com>
2019-07-10 14:12:33 +0900

    F

---
+++ ABC/052/FactorsofFractorial.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+const int mod = 1e9+7;
+
+int main(void){
+
+	int N; cin >> N;
+
+	int ans = 0;
+	while(N > 0){
+		ans += 
+	}
+
+	
+
+
+	return 0;
+
+}
