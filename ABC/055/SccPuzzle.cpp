2c0f8e7d29bb5407f7ef11fb753144ea7d44675a
hntk03 <hntk03@gmail.com>
2020-02-04 13:47:59 +0900

    SccPuzzle

--- ABC/055/SccPuzzle.cpp
+++ ABC/055/SccPuzzle.cpp
@@ -1,12 +1,7 @@
 #include <bits/stdc++.h>
 
-
-
 using namespace std;
-
-typedef vector<int> VI;
-typedef vector<string> VS;
-
+using ll = long long;
 
 //container util
 #define SORT(c) sort((c).begin(),(c).end())
@@ -15,24 +10,26 @@ typedef vector<string> VS;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
 
-
+const int INF = 1e9;
 
 int main(void){
 
-	int N, M; cin >> N >> M;
+	ll N, M; cin >> N >> M;
 
-	int ans = 0;
-	if((M/2) >= N){
-		ans += (N);
-		M -= (2*N);
-		ans += (M/4);
+	ll ans = 0;
+	if((M/2)<N){
+		ans += M/2;
 	}else{
-		ans += (M/2);
-		M -= (M/2)*2;
-		ans += (M/4);
+		ans += N;
+		M -= N*2;
+		ans += M / 4;
 	}
 
 	cout << ans << endl;
+
+
+	
+
 
 	return 0;
 
