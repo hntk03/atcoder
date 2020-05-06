8b49fcd454144d505742dc48d69a25090a64ca6e
hntk03 <hntk03@gmail.com>
2020-05-06 18:05:34 +0900

    IceTeaStore

--- AGC/019/IceTeaStore.cpp
+++ AGC/019/IceTeaStore.cpp
@@ -10,34 +10,19 @@ using ll = long long;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
 
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
 
 int main(void){
 
-	int Q, H, S, D; cin >> Q >> H >> S >> D;
-	int N; cin >> N;
+	ll Q, H, S, D; cin >> Q >> H >> S >> D;
+	ll N; cin >> N;
 
-	vector<pair<double, int>> v;
-	v.push_back(make_pair(4.0*Q, 1));
-	v.push_back(make_pair(2.0*H, 2));
-	v.push_back(make_pair(1.0*S, 3));
-	v.push_back(make_pair(0.5*D, 4));
+	ll x = min(min(4*Q, 2*H), S);
 
-	SORT(v);
-
-	ll ans = 0;
-	if(v[0].second == 4){
-		if(N%2 == 0) ans += D * (N / 2);
-		else{
-			int c = (N / 2);
-			ans += D * c;
-			N -= 2*c;
-			ans += N * v[1].first;
-		}
-	}else{
-		ans += N * v[0].first;
-	}
-
-
+	ll ans = min(D*(N/2)+x*(N%2), x*N);
 	cout << ans << endl;
 
 
