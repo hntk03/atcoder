7d871b966ce46ef7f132cbbc4764dccc42226e98
hntk03 <hntk03@gmail.com>
2020-07-12 14:55:15 +0900

    aising2020

---
+++ company/aising2020/NumberofMultiples.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	int L, R, d; cin >> L >> R >> d;
+
+	int ans = 0;
+	FOR(i,L,R+1){
+		if(i%d == 0) ans++;
+	}
+
+	cout << ans << endl;
+
+	return 0;
+
+}
