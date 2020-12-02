c5bc09b53da1ffbb7e9a821ab5f9cc0cce93868b
hntk03 <hntk03@gmail.com>
2020-12-02 15:53:42 +0900

    184

---
+++ ABC/184/Quizzes.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	ll N, X; cin >> N >> X;
+	string S; cin >> S;
+
+	for(ll i=0;i<N;i++){
+		if(X != 0){
+		if(S[i] == 'o') X++;
+		else X--;
+		}
+	}
+
+	cout << X << endl;
+
+	return 0;
+
+}
