3b24e26e8c617a0197e85542bdfc7663839524e3
hntk03 <hntk03@gmail.com>
2020-12-14 12:30:11 +0900

    185

---
+++ ABC/185/ABCPreparation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	vector<int> A(4);
+	REP(i,4) cin >> A[i];
+
+	int ans = 10000;
+	REP(i,4)  ans = min(ans, A[i]);
+
+
+	cout << ans << endl;
+	return 0;
+
+}
