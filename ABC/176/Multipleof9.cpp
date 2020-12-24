ed103bb083891102f5276c88a0452ea0fd2194b9
hntk03 <hntk03@gmail.com>
2020-12-24 13:56:54 +0900

    176

---
+++ ABC/176/Multipleof9.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	string S; cin >> S;
+	int N = S.length();
+
+	ll ans = 0;
+	REP(i,N) ans += S[i] - '0';
+
+	if(ans%9 == 0) cout << "Yes" << endl;
+	else cout << "No" << endl;
+	
+
+
+	return 0;
+
+}
