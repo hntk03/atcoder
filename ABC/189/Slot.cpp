70576c65208af73f3658034c8c6227131bf85115
hntk03 <hntk03@gmail.com>
2021-01-28 16:17:29 +0900

    189

---
+++ ABC/189/Slot.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	string s; cin >> s;
+
+	if(s[0] == s[1] and s[1] == s[2]) cout << "Won" << endl;
+	else cout << "Lost" << endl;
+
+
+	return 0;
+
+}
