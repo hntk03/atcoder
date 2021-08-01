285cddc524354bc5fa2fbcdc6d44f322b22c1a78
hntk03 <hntk03@gmail.com>
2021-08-01 12:55:43 +0900

    204

---
+++ ABC/203/AtCoderCondominium.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	int N, K; cin >> N >> K;
+
+	int ans = 0;
+	REP(i,N){
+		REP(j,K){
+			ans += 100*(i+1) + j+1;
+		}
+	}
+
+
+	cout << ans << endl;
+	return 0;
+
+}
---
+++ ABC/204/Nuts.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	int N; cin >> N;
+	vector<int> A(N);
+
+	REP(i,N) cin >> A[i];
+	int ans = 0;
+
+	REP(i,N){
+		if(A[i] > 10) ans += A[i] - 10;
+	}
+
+	cout << ans << endl;
+
+
+	return 0;
+
+}
