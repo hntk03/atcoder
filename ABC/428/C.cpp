e581f1e75a53bbc5bebb7989ee823355daf12a3f
hntk03 <hntk03@gmail.com>
2025-11-08 21:47:57 +0900

    428

--- ABC/428/C.cpp
+++ ABC/428/C.cpp
@@ -3,31 +3,34 @@
 using namespace std;
 using ll = long long;
 
-//container util
-#define SORT(c) sort((c).begin(),(c).end())
+// container util
+#define SORT(c) sort((c).begin(), (c).end())
 #define all(a) (a).begin(), (a).end()
 
-//repetition
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
+// repetition
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
 
-//print
+// print
 #define pv(val) cerr << #val << '=' << (val) << endl
 
 const int INF = 1e9;
 
-int main(void){
-
-  int Q; cin >> Q;
+int main(void) {
+  int Q;
+  cin >> Q;
   vector<int> x = {0};
-  REP(i,Q){
-    int type; cin >> type;
-    if(type == 1){
-      char c; cin >> c;
+  REP(i, Q) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      char c;
+      cin >> c;
       int nx = x.back() + (c == '(' ? 1 : -1);
-      if(nx < 0) nx = -INF;
+      if (nx < 0)
+        nx = -INF;
       x.push_back(nx);
-    }else{
+    } else {
       x.pop_back();
     }
 
