67521c83d10959eb885abf5f702c6e777ac3aea5
hntk03 <hntk03@gmail.com>
2025-11-08 19:38:01 +0900

    429

--- ABC/429/B.cpp
+++ ABC/429/B.cpp
@@ -3,37 +3,36 @@
 using namespace std;
 using ll = long long;
 
-//container util
-#define SORT(c) sort((c).begin(),(c).end())
+// container util
+#define SORT(c) sort((c).begin(), (c).end())
 #define all(a) (a).begin(), (a).end()
 
-//repetition
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
+// repetition
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
 
-//print
+// print
 #define pv(val) cerr << #val << '=' << (val) << endl
 
 const int INF = 1e9;
 
-int main(void){
-
-  int N, M; cin >> N >> M;
+int main(void) {
+  int N, M;
+  cin >> N >> M;
   vector<int> A(N);
-  REP(i,N) cin >> A[i];
+  REP(i, N) cin >> A[i];
 
   int sum = 0;
-  REP(i,N) sum += A[i];
+  REP(i, N) sum += A[i];
 
-  REP(i,N){
+  REP(i, N) {
     int now = sum - A[i];
-    if(now == M){
+    if (now == M) {
       cout << "Yes" << endl;
       return 0;
     }
   }
-  
+
   cout << "No" << endl;
   return 0;
-
 }
--- ABC/429/C.cpp
+++ ABC/429/C.cpp
@@ -3,39 +3,38 @@
 using namespace std;
 using ll = long long;
 
-//container util
-#define SORT(c) sort((c).begin(),(c).end())
+// container util
+#define SORT(c) sort((c).begin(), (c).end())
 #define all(a) (a).begin(), (a).end()
 
-//repetition
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
+// repetition
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
 
-//print
+// print
 #define pv(val) cerr << #val << '=' << (val) << endl
 
 const int INF = 1e9;
 
-ll c2(ll n){
-  return n*(n-1)/2;
+ll c2(ll n) {
+  return n * (n - 1) / 2;
 }
 
-int main(void){
-
-  int N; cin >> N;
+int main(void) {
+  int N;
+  cin >> N;
   vector<int> A(N);
-  REP(i,N) cin >> A[i];
-  REP(i,N) A[i]--;
+  REP(i, N) cin >> A[i];
+  REP(i, N) A[i]--;
 
   vector<int> cnt(N);
-  REP(i,N) cnt[A[i]]++;
+  REP(i, N) cnt[A[i]]++;
 
   ll ans = 0;
-  REP(i,N){
-    ans += c2(cnt[i])*(N-cnt[i]);
+  REP(i, N) {
+    ans += c2(cnt[i]) * (N - cnt[i]);
   }
 
   cout << ans << endl;
   return 0;
-
 }
