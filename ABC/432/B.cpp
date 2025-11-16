df97c701baa7a28628e29ca4e4a37e0c3b19b58b
hntk03 <hntk03@gmail.com>
2025-11-16 12:29:58 +0900

    432

---
+++ ABC/432/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+// container util
+#define SORT(c) sort((c).begin(), (c).end())
+#define all(a) (a).begin(), (a).end()
+
+// repetition
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+// print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void) {
+  string X;
+  cin >> X;
+
+  SORT(X);
+
+  if (X.front() == '0') {
+    int idx = 1;
+    while (idx < X.size() and X[idx] == '0')
+      idx++;
+    swap(X[0], X[idx]);
+  }
+
+  cout << X << endl;
+  return 0;
+}
